932ee775bd25014c33687fe6c03b978c2ad474d8
Malvin <malvinsoncoding@gmail.com>
2025-06-02 21:24:10 +0700

    fix eslint

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -52,7 +52,7 @@ export default function Login() {
         </button>
       </form>
       <p className="mt-4 text-center">
-        {"Don't have an account?"}
+        Don&apos;t have an account?&nbsp;
         <a href="/signup" className="text-blue-500">
           Sign Up
         </a>
